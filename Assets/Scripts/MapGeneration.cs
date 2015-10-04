10f36b5e795d04384d00bbdfb8fb6a338abcf947
Teresa di Tada <tditada@gmail.com>
2015-10-04 03:51:42 -0300

    mount point in chunks and list of chunks

--- Assets/Scripts/MapGeneration.cs
+++ Assets/Scripts/MapGeneration.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 
 public class MapGeneration : MonoBehaviour {
-
+	
 	public List<GameObject> chunks = new List<GameObject>();
 
 	public void newMap(int rooms){
