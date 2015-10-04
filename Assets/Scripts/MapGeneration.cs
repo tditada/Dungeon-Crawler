538c3c10a5b6191188797f8fe2aef72162a0cb66
Teresa di Tada <tditada@gmail.com>
2015-10-04 03:42:39 -0300

    scenes change

--- Assets/Scripts/MapGeneration.cs
+++ Assets/Scripts/MapGeneration.cs
@@ -1,9 +1,12 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class MapGeneration : MonoBehaviour {
 
+	public List<GameObject> chunks = new List<GameObject>();
+
 	public void newMap(int rooms){
-		Debug.Log ("new map");
+		Debug.Log ("new map"); 
 	}
 }
