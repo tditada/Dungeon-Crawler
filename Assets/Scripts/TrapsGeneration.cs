f50024c1fb03c566e1c618276ec0f82c60b63dce
Tere <tditada@gmail.com>
2015-10-11 04:08:49 -0300

    instantiation, to test

--- Assets/Scripts/TrapsGeneration.cs
+++ Assets/Scripts/TrapsGeneration.cs
@@ -6,6 +6,7 @@ public class TrapsGeneration : MonoBehaviour {
 	
 	private Dictionary<Vector2, GameObject> map = new Dictionary<Vector2, GameObject>();
 	public int probability_room;
+	public List<GameObject> traps = new List<GameObject>();
 
 	void setMap(Dictionary<Vector2, GameObject> map){
 		this.map = map;
@@ -17,7 +18,10 @@ public class TrapsGeneration : MonoBehaviour {
 			ChunckScript cs = value.GetComponent<ChunckScript>();
 			if(Random.value > probability_room){
 				List<Vector3> v3 = cs.returnPossibleTraps();
-				Vector3 trap = v3[Random.Range(0,v3.Count)]; //TODO: activate trap
+				Vector3 trapPosition = v3[Random.Range(0,v3.Count)]; 
+				GameObject p = traps[Random.Range(0,traps.Count)];
+				Instantiate(p,trapPosition,p.transform.rotation);
+
 			}
 
 		}
