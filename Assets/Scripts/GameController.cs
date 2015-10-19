a30067ef76da231b38ad4bc638b85f70622b12b3
Tere <tditada@gmail.com>
2015-10-19 03:41:43 -0300

    pause in Demo Scene

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+	public void Pause(){
+		if (Time.timeScale==1) {
+			Time.timeScale = 0;
+//			uiController.PauseMenu(true);
+		} else {
+			Time.timeScale =1;
+//			uiController.PauseMenu(false);
+		}
+	}
+}
