df7375f414c5da5f6c52da40a6b1829464e1519f
Facundo Martinez <fnmartinez88@gmail.com>
2015-10-19 10:50:01 -0300

    Se saca el directional light de la DemoScene y se pone todo para probar el resto del script

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -5,27 +5,42 @@ using System.Collections.Generic;
 
 public class MapGenerator : MonoBehaviour {
 
-	public TwoWayCorridorChunk twoWayCorridorPrefab;
-	public ThreeWayCorridorChunk threeWayCorridorPrefab;
-	public FourWayCorridorChunk fourWayCorridorPrefab;
-	public CornerChunk cornerPrefab;
-	public ChamberChunk chamberPrefab;
-	public CornerChamberChunk cornerChamberPrefab;
-	public TwoWayChamberChunk twoWayChamberPrefab;
-	public ThreeWayChamberChunk threeWayChamberChunk;
-	public FourWayChamberChunk fourWayChamberChunk;
+	//public TwoWayCorridorChunk twoWayCorridorPrefab;
+	//public ThreeWayCorridorChunk threeWayCorridorPrefab;
+	//public FourWayCorridorChunk fourWayCorridorPrefab;
+	//public CornerChunk cornerPrefab;
+	//public ChamberChunk chamberPrefab;
+	//public CornerChamberChunk cornerChamberPrefab;
+	//public TwoWayChamberChunk twoWayChamberPrefab;
+	//public ThreeWayChamberChunk threeWayChamberPrefab;
+	//public FourWayChamberChunk fourWayChamberPrefab;
+	public Chunk twoWayCorridorPrefab;
+	public Chunk threeWayCorridorPrefab;
+	public Chunk fourWayCorridorPrefab;
+	public Chunk cornerPrefab;
+	public Chunk chamberPrefab;
+	public Chunk cornerChamberPrefab;
+	public Chunk twoWayChamberPrefab;
+	public Chunk threeWayChamberPrefab;
+	public Chunk fourWayChamberPrefab;
+	public double chunkSize = 34.8;
 
-	private Dictionary<Point, Chunk> map = new Dictionary<Point, Chunk>();
+	public double ramificationProbability = 0.5;
+	public double chamberProbability = 0.2;
+
+	private Dictionary<Point, Chunk> map;
 	private Point startPoint;
 	private Point endPoint;
 	private System.Random random;
 
-	public void newMap(int maxSize, int minSteps, int maxSteps, int seed, double ramificationProbability) {
+	public Dictionary<Point, Chunk> NewMap(int maxSize, int minSteps, int maxSteps, int seed) {
 		if (minSteps < maxSteps) { throw new ArgumentException(); }
 		if (minSteps > maxSize) { throw new ArgumentException(); }
 		if (maxSteps > maxSize) { throw new ArgumentException(); }
 		if (ramificationProbability < 0.0 || ramificationProbability > 1.0) { throw new ArgumentException(); }
+		if (chamberProbability < 0.0 || ramificationProbability > 1.0) { throw new ArgumentException(); }
 
+		map = new Dictionary<Point, Chunk>();
 		startPoint = new Point(random.Next(0, maxSize), random.Next(0, maxSize));
 		endPoint = GetEndPoint(startPoint, minSteps, maxSteps, maxSize);
 
@@ -38,7 +53,10 @@ public class MapGenerator : MonoBehaviour {
 
 		GenerateTrivialPath(startPoint, endPoint);
 
-		Ramify(ramificationProbability, maxSize);
+		Ramify(ramificationProbability, chamberProbability, maxSize);
+
+		GenerateMap();
+		return map;
 	}
 
 	Point GetEndPoint (Point startPoint, int minSteps, int maxSteps, int maxSize) {
@@ -104,29 +122,29 @@ public class MapGenerator : MonoBehaviour {
 		}
 	}
 
-	void Ramify (double ramificationProbability, int maxSize) {
+	void Ramify (double ramificationProbability, double chamberProbability, int maxSize) {
 		foreach (var chunk in map) {
-			RamifyChunk(null, chunk.Key, ramificationProbability, maxSize);
+			RamifyChunk(null, chunk.Key, ramificationProbability, chamberProbability, maxSize);
 		}
 	}
 
-	bool RamifyChunk (Point previousPosition, Point currentPosition, double ramificationProbability, int maxSize) {
+	bool RamifyChunk (Point previousPosition, Point currentPosition, double ramificationProbability, double chamberProbability, int maxSize) {
 		if (!map.ContainsKey(currentPosition) && random.NextDouble() < ramificationProbability) {
 			var ramificationPoints = GetRamificationPoints(currentPosition, maxSize);
 			for (int i = 0; i < ramificationPoints.Count; i++) {
 				map.Add(ramificationPoints[i], null);
-				if (!RamifyChunk(currentPosition, ramificationPoints[i], ramificationProbability, maxSize)) {
+				if (!RamifyChunk(currentPosition, ramificationPoints[i], ramificationProbability, chamberProbability, maxSize)) {
 					ramificationPoints.Remove(ramificationPoints[i]);
 					i--;
 				}
 			}
-			DecideChunk(previousPosition, currentPosition, ramificationPoints);
+			DecideChunk(previousPosition, currentPosition, chamberProbability, ramificationPoints);
 			return true;
 		}
 		return false;
 	}
 
-	void DecideChunk (Point previousPosition, Point currentPosition, List<Point> ramificationPoints) {
+	void DecideChunk (Point previousPosition, Point currentPosition, double chamberProbability, List<Point> ramificationPoints) {
 		var chunk = map[currentPosition];
 		switch (ramificationPoints.Count) {
 		case 0:
@@ -136,27 +154,25 @@ public class MapGenerator : MonoBehaviour {
 			break;
 		case 1:
 			if (chunk == null) {
-				chunk = PutTwoWayChunk(currentPosition, previousPosition, ramificationPoints[0]);
+				chunk = PutTwoWayChunk(currentPosition, previousPosition, ramificationPoints[0], chamberProbability);
 			} else {
 				Destroy(chunk);
-				chunk = PutThreeWayChunk(currentPosition, previousPosition, ramificationPoints[0], ramificationPoints[1]);
-				chunk = Instantiate(threeWayCorridorPrefab) as Chunk;
+				chunk = PutThreeWayChunk(currentPosition, previousPosition, ramificationPoints[0], ramificationPoints[1], chamberProbability);
 			}
 			break;
 		case 2:
 			if (chunk == null) {
-				chunk = PutThreeWayChunk(currentPosition, previousPosition, ramificationPoints[0], ramificationPoints[1]);
-				chunk = Instantiate(threeWayCorridorPrefab) as Chunk;
+				chunk = PutThreeWayChunk(currentPosition, previousPosition, ramificationPoints[0], ramificationPoints[1], chamberProbability);
 			} else {
 				Destroy(chunk);
-				chunk = Instantiate(fourWayCorridorPrefab) as Chunk;
+				chunk = PutFourWayChunk(chamberProbability);
 			}
 			break;
 		case 3:
 			if (chunk != null) {
 				Destroy(chunk);
 			}
-			chunk = Instantiate(fourWayCorridorPrefab) as Chunk;
+			chunk = PutFourWayChunk(chamberProbability);
 			break;
 		}
 		map[currentPosition] = chunk;
@@ -168,19 +184,19 @@ public class MapGenerator : MonoBehaviour {
 		return chunk;
 	}
 
-	Chunk PutTwoWayChunk (Point currentPosition, Point previousPosition, Point other) {
+	Chunk PutTwoWayChunk (Point currentPosition, Point previousPosition, Point other, double chamberProbability) {
 		Chunk chunk = null;
 		if ((((int)previousPosition.RelativeOrientationTo(currentPosition) -
 		      (int)other.RelativeOrientationTo(currentPosition)) % 2) == 0) {
-			chunk = PutSraightTwoWayChunk(currentPosition, previousPosition, other);
+			chunk = PutSraightTwoWayChunk(currentPosition, previousPosition, other, chamberProbability);
 		} else {
-			chunk = PutCornerTwoWayChunk(currentPosition, previousPosition, other);
+			chunk = PutCornerTwoWayChunk(currentPosition, previousPosition, other, chamberProbability);
 	    }
 		return chunk;
 	}
 
-	Chunk PutSraightTwoWayChunk (Point currentPosition, Point previousPosition, Point other) {
-		Chunk chunk = Instantiate(twoWayCorridorPrefab) as Chunk;
+	Chunk PutSraightTwoWayChunk (Point currentPosition, Point previousPosition, Point other, double chamberProbability) {
+		Chunk chunk = InstantiateChunk(twoWayCorridorPrefab, twoWayChamberPrefab, chamberProbability); 
 		if (previousPosition.RelativeOrientationTo(currentPosition) == Orientation.SOUTH ||
 		    previousPosition.RelativeOrientationTo(currentPosition) == Orientation.NORTH) {
 			chunk.Rotate(Orientation.NORTH);						
@@ -190,8 +206,8 @@ public class MapGenerator : MonoBehaviour {
 		return chunk;
 	}
 
-	Chunk PutCornerTwoWayChunk (Point currentPosition, Point previousPosition, Point other) {
-		Chunk chunk = Instantiate(cornerPrefab) as Chunk;
+	Chunk PutCornerTwoWayChunk (Point currentPosition, Point previousPosition, Point other, double chamberProbability) {
+		Chunk chunk = InstantiateChunk(cornerPrefab, cornerChamberPrefab, chamberProbability);
 		if ((previousPosition.RelativeOrientationTo(currentPosition) == Orientation.NORTH &&
 		     other.RelativeOrientationTo(currentPosition) == Orientation.EAST) ||
 		    (previousPosition.RelativeOrientationTo(currentPosition) == Orientation.EAST &&
@@ -216,8 +232,8 @@ public class MapGenerator : MonoBehaviour {
 		return chunk;
 	}
 
-	Chunk PutThreeWayChunk (Point currentPosition, Point previousPosition, Point other1, Point other2) {
-		Chunk chunk = Instantiate(threeWayCorridorPrefab) as Chunk;
+	Chunk PutThreeWayChunk (Point currentPosition, Point previousPosition, Point other1, Point other2, double chamberProbability) {
+		Chunk chunk = InstantiateChunk(threeWayCorridorPrefab, threeWayChamberPrefab, chamberProbability);
 		var orientationSum = (int)previousPosition.RelativeOrientationTo(currentPosition) +
 			(int)other1.RelativeOrientationTo(currentPosition) + (int)other2.RelativeOrientationTo(currentPosition);
 		if (orientationSum % 2 == 0) {
@@ -240,6 +256,18 @@ public class MapGenerator : MonoBehaviour {
 		return chunk;
 	}
 
+	Chunk PutFourWayChunk(double chamberProbability) {
+		return InstantiateChunk(fourWayCorridorPrefab, fourWayChamberPrefab, chamberProbability);
+	}
+
+	Chunk InstantiateChunk(Chunk corridorChunk, Chunk chamberChunk, double chamberProbability) {
+		if (random.NextDouble() < chamberProbability) {
+			return Instantiate(chamberChunk) as Chunk;
+		} else {
+			return Instantiate(corridorChunk) as Chunk;
+		}
+	}
+
 	List<Point> GetRamificationPoints (Point center, int maxSize) {
 		var ramificationPoints = new List<Point>();
 		var xs = new int[] {1, 0, -1, 0};
@@ -256,5 +284,12 @@ public class MapGenerator : MonoBehaviour {
 	bool UsablePoint (Point p, int maxSize) {
 		return p.X >=0 && p.X < maxSize && p.Y >= 0 && p.Y < maxSize && !map.ContainsKey(p);
 	}
-
+	
+	void GenerateMap() {
+		foreach (Point p in map.Keys) {
+			Chunk c = map[p];
+			c.gameObject.transform.position = new Vector3((float)(p.Y * chunkSize), 0.0f, (float)(p.X * chunkSize));
+			c.enabled = true;
+		}
+	}
 }
