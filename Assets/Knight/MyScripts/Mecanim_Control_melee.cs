ded721f63eef1cd424c31781ef9928d3308dcfa3
Tere <tditada@gmail.com>
2015-10-20 01:26:34 -0300

    no more attack and defense in player

--- Assets/Knight/MyScripts/Mecanim_Control_melee.cs
+++ Assets/Knight/MyScripts/Mecanim_Control_melee.cs
@@ -4,11 +4,11 @@ using System.Collections.Generic;
 
 public class Mecanim_Control_melee : MonoBehaviour {
 	public Animator animator;
-	public bool leftMouseClick=false;
-	public bool rightMouseClick=false;
+//	public bool leftMouseClick=false;
+//	public bool rightMouseClick=false;
 	public bool canControl=true;
 	private float shift_axis_late;
-	public float leftMouseClicks;
+//	public float leftMouseClicks;
 
 	private float animLayer2;
 	public float inputX;
@@ -42,23 +42,23 @@ public class Mecanim_Control_melee : MonoBehaviour {
 	
 	void Update () {
 	
-		if(leftMouseClick){
-			StartCoroutine("TimerClickTime");
-
-		}
+//		if(leftMouseClick){
+//			StartCoroutine("TimerClickTime");
+//
+//		}
 		
 		if(animator){	
 			
 			shift_axis_late = Mathf.Clamp((shift_axis_late - 0.005f), 0.0f, 1.1f);
 			animLayer2 = Mathf.Clamp((animLayer2 - 0.01f), 0.0f, 1.0f);
 			
-			animator.SetBool("LeftMouseClick", leftMouseClick);
+//			animator.SetBool("LeftMouseClick", leftMouseClick);
 			
 			animator.SetFloat("LeftShift_axis", shift_axis_late);
 			animator.SetFloat("Axis_Horizontal", inputX);
 			animator.SetFloat("Axis_Vertical", inputY);
 			animator.SetFloat("Jump_axis", inputJump);
-			animator.SetBool("RightMouse", rightMouseClick);
+//			animator.SetBool("RightMouse", rightMouseClick);
 
 		}
 		
@@ -69,7 +69,7 @@ public class Mecanim_Control_melee : MonoBehaviour {
 			inputX = Input.GetAxis("Horizontal");
 			inputY = Input.GetAxis("Vertical");
 			inputJump = Input.GetAxis("Jump");
-			leftMouseClick = Input.GetMouseButtonDown(0);
+//			leftMouseClick = Input.GetMouseButtonDown(0);
 		
 			
 	
@@ -79,14 +79,14 @@ public class Mecanim_Control_melee : MonoBehaviour {
 			
 		}
 		
-	
-		if(Input.GetAxis("Fire2")>0){
-				rightMouseClick=true;
-				animLayer2=0.5f;
-		}
-			else{
-				rightMouseClick=false;
-			}	
+//	
+//		if(Input.GetAxis("Fire2")>0){
+//				rightMouseClick=true;
+//				animLayer2=0.5f;
+//		}
+//			else{
+//				rightMouseClick=false;
+//			}	
 		
 
 
@@ -105,25 +105,25 @@ public class Mecanim_Control_melee : MonoBehaviour {
 
 
 
-	void FightCombo(){   //every left mouse click +1 to animation number counter
+//	void FightCombo(){   //every left mouse click +1 to animation number counter
+//
+//		leftMouseClicks += 1f;
+//		animator.SetFloat("LeftMouseClicks", leftMouseClicks);
+//
+//		if(leftMouseClicks>2f){
+//			leftMouseClicks = 0f;
+//		}
+//
+//	}	
 
-		leftMouseClicks += 1f;
-		animator.SetFloat("LeftMouseClicks", leftMouseClicks);
-
-		if(leftMouseClicks>2f){
-			leftMouseClicks = 0f;
-		}
-
-	}	
-
-	IEnumerator TimerClickTime(){  //timer, few seconds after click mouse bool leftMouseClick = true
-	
-		yield return new WaitForSeconds(0.1f);
-		leftMouseClick=false;
-		yield return null;
-	
-	}
-	
+//	IEnumerator TimerClickTime(){  //timer, few seconds after click mouse bool leftMouseClick = true
+//	
+//		yield return new WaitForSeconds(0.1f);
+//		leftMouseClick=false;
+//		yield return null;
+//	
+//	}
+//	
 	
 	IEnumerator InAction(){ //recieve message from fight animation in mecanim controller
 		
