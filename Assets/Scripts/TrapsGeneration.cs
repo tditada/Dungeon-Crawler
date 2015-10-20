d8d7c2608f135100707e048d8c9697576e61f40b
Tere <tditada@gmail.com>
2015-10-20 20:51:32 -0300

    random number of traps per chunck

--- Assets/Scripts/TrapsGeneration.cs
+++ Assets/Scripts/TrapsGeneration.cs
@@ -13,19 +13,19 @@ public class TrapsGeneration : MonoBehaviour {
 		Debug.Log ("generate trap");
 		foreach (KeyValuePair<Point, Chunk> kvp in map) {
 			Chunk value = kvp.Value;
-			int i = Random.Range (0,traps.Count);
+			List<Vector3> possibleTraps = value.returnPossibleTraps();
+			int i = Random.Range (0,possibleTraps.Count);
 			while(i>=0){
 				if(Random.value < probability_room){
 					Debug.Log (value);
-					List<Vector3> possibleTraps = value.returnPossibleTraps();
 					Vector3 trapPosition = possibleTraps[Random.Range(0,possibleTraps.Count)]; //- new Vector3(kvp.Key.X, 0); 
 					Debug.Log (trapPosition);
-					GameObject p = traps[Random.Range(0,traps.Count-1)];
+					GameObject p = traps[Random.Range(0,traps.Count)];
 					Object o = Instantiate(p,Vector3.zero,p.transform.rotation);
 					((GameObject)o).gameObject.transform.parent=value.gameObject.transform;
 					((GameObject)o).gameObject.transform.localPosition=trapPosition;
 				}
-				i++;
+				i--;
 			}
 		}
 	}
