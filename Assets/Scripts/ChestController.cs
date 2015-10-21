36e982deddb46f352f254c0b9dbdc7e1928ed8d0
Matias Domingues <mannias@gmail.com>
2015-10-21 19:26:08 -0300

    added chest controller

---
+++ Assets/Scripts/ChestController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChestController : MonoBehaviour {
+
+	GameController gameController;
+	
+	void Start() {
+		GameObject controllerObject = GameObject.FindGameObjectWithTag ("GameController");
+		gameController = controllerObject.GetComponent<GameController> ();
+	}
+	
+	void OnTriggerEnter(Collider other) {
+		gameController.Win ();
+	}
+
+}
