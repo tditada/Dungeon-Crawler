c0adfa960fe224fe1a5479a4d8367c6d60b10a97
Tere <tditada@gmail.com>
2015-10-20 22:51:24 -0300

    map and trap generation and setting player and treasure

---
+++ Assets/Scripts/StartingSets.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class StartingSets : MonoBehaviour {
+
+	Chunk startPoint;
+	Chunk endPoint;
+	public GameObject treasure;
+
+	private void setMap(Dictionary<Point, Chunk> map){		
+		foreach (KeyValuePair<Point, Chunk> kvp in map) {
+			if(kvp.Value.isStartChunk){
+				startPoint=kvp.Value;
+			}else if(kvp.Value.isEndChunk){
+				endPoint=kvp.Value;
+			}
+		}
+	}
+
+	private void setPlayer(){
+		GameObject player = GameObject.FindGameObjectWithTag("Player");
+		player.transform.position = startPoint.gameObject.transform.position;
+	}
+
+	private void setTreasure(){
+		Instantiate (treasure,endPoint.gameObject.transform.position, endPoint.gameObject.transform.rotation);
+	}
+
+	public void startingGame(Dictionary<Point, Chunk> map){
+		setMap (map);
+		setPlayer ();
+		setTreasure ();
+	}
+
+}
--- Assets/Scripts/animation.js
+++ Assets/Scripts/animation.js
@@ -1,6 +1,6 @@
 
 
-function OnMouseEnter () {
+function OnTriggerEnter (other : Collider) {
     GetComponent.<Animation>().Play("open");
     yield WaitForSeconds (1.75);
     GetComponent.<Animation>().Play("close");
